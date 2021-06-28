736318d3f4ad279bd21a397a7b9778de4d54f049
Daniel Goz <daniel@dgoz.net>
2021-06-28 00:58:59 -0400

    nixos: switch to pulsewire

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -37,9 +37,14 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  # Enable sound.through pipewire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dansman805 = {
