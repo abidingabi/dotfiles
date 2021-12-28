8825f2d598eb0f39a35170c72a9518e3a9eb8bc3
Daniel Goz <daniel@dgoz.net>
2021-12-28 15:12:57 -0500

    nixos: fix interception-tools
    See https://github.com/NixOS/nixpkgs/issues/126681

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,9 +60,20 @@
     dconf
   ];
 
-  services.interception-tools.enable = true;
   services.picom.enable = true;
   services.earlyoom.enable = true;
+
+  # Map CapsLock to Esc on single press and Ctrl on when used with multiple keys.
+  services.interception-tools = {
+    enable = true;
+    plugins = [ pkgs.interception-tools-plugins.caps2esc ];
+    udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
 
   system.stateVersion = "20.09"; # Did you read the comment?
 }
