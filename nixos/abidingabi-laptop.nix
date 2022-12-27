8012ad4296b43e68a8602aa881d66d6486ae22e7
Abigail <abigail@dogbuilt.net>
2022-12-27 18:13:05 -0500

    nixos: set laptop kernel version to 6.0
    See https://bugs.archlinux.org/task/76795

--- nixos/abidingabi-laptop.nix
+++ nixos/abidingabi-laptop.nix
@@ -3,6 +3,8 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
+  boot.kernelPackages = pkgs.linuxPackages_6_0;
+
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" "nvme" ];
   boot.initrd.kernelModules = [ ];
