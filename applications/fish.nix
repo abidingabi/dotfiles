0579cd7505f8a6e03046d4d847d9d45ee98c421d
Abigail <abigail@dogbuilt.net>
2023-04-24 12:16:09 -0400

    fish: port over config

---
+++ applications/fish.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  hmModules = [{
+    programs.fish = {
+      enable = true;
+
+      shellInit = "set -x EDITOR 'vim'";
+
+      functions = {
+        fish_greeting = "";
+        fish_prompt = ''
+          test $SSH_TTY
+               and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+          test "$USER" = 'root'
+               and echo (set_color red)"#"
+
+          # Main
+          echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+        '';
+
+        upload_file = "curl http://0x0.st -F 'file=@'$argv";
+      };
+    };
+  }];
+}
