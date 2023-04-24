12f8fb32868584608db98f6193371cb166e2977e
Abigail <abigail@dogbuilt.net>
2023-04-24 13:39:13 -0400

    git: port over config

---
+++ applications/git.nix
@@ -0,0 +1,30 @@
+{
+  hmModules = [{
+    programs.git = {
+      enable = true;
+
+      userName = "Abigail";
+      userEmail = "abigail@dogbuilt.net";
+
+      ignores = [ "*~" "*.swp" ".envrc" ];
+
+      aliases = {
+        pushf = "push --force-with-lease";
+      };
+
+      extraConfig = {
+        merge.ff = "only";
+        branch.autosetuprebase = "always";
+        pull.rebase = true;
+        rebase = {
+          autoStash = true;
+          autosquash = true;
+        };
+
+        init.defaultBranch = "main";
+
+        github.user = "abidingabi";
+      };
+    };
+  }];
+}
