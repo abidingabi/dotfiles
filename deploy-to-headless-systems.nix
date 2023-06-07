902763be7b3379270ac2ed7b64152aad5ba16262
Abigail <abigail@dogbuilt.net>
2023-06-07 19:28:51 -0400

    nixos: fix relative path in deploy script

--- deploy-to-headless-systems.nix
+++ deploy-to-headless-systems.nix
@@ -1,3 +1,3 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 echo "lily:"
-nixos-rebuild switch --no-build-nix --target-host root@5.161.71.187 --flake .#lily
+nixos-rebuild switch --no-build-nix --target-host root@5.161.71.187 --flake $(dirname "${BASH_SOURCE[0]}")#lily
