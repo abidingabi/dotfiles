3aee2944a8b33e94a76be804e61a8480c85bc48e
Abigail <abigail@dogbuilt.net>
2023-06-25 21:08:49 -0400

    fish: add FIRST-related API query functions

--- applications/fish.nix
+++ applications/fish.nix
@@ -28,6 +28,24 @@
         '';
 
         upload_file = "curl http://0x0.st -F 'file=@'$argv";
+
+        # FIRST-related APIs
+        query_ftc_events = ''
+          set FTC_EVENTS_KEY (cat ~/ftc-events.key)
+          set BASE_API_URL "https://ftc-api.firstinspires.org/v2.0"
+          curl -H 'Accept:application/json' -H "Authorization: Basic $FTC_EVENTS_KEY" "$BASE_API_URL/$argv"
+        '';
+
+        query_frc_events = ''
+          set FRC_EVENTS_KEY (cat ~/frc-events.key)
+          set BASE_API_URL "https://frc-api.firstinspires.org/v2.0"
+          curl -H 'Accept:application/json' -H "Authorization: Basic $FRC_EVENTS_KEY" "$BASE_API_URL/$argv"
+        '';
+
+        query_toa = ''
+          set TOA_KEY (cat ~/theorangealliance.key)
+          curl -H "X-TOA-Key:$TOA_KEY" -H "Content-Type:application/json" -H "X-Application-Origin:abidingabi-interactive-api-usage" "https://theorangealliance.org/api/$argv"
+        '';
       };
     };
   }];
