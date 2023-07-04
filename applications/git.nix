9f79a8a4edeb8db80f2f791ec4d2273177f75ab0
Abigail <abigail@dogbuilt.net>
2023-07-04 12:45:14 -0400

    git: add global mailmap

--- applications/git.nix
+++ applications/git.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   hmModules = [{
     programs.git = {
@@ -22,6 +24,12 @@
         init.defaultBranch = "main";
 
         github.user = "abidingabi";
+
+        mailmap.file = toString (pkgs.writeText "mailmap" ''
+          Abigail <abigail@dogbuilt.net>
+          Abigail <abigail@dogbuilt.net> <daniel@dgoz.net>
+          Abigail <abigail@dogbuilt.net> <dansman805@gmail.com>
+        '');
       };
     };
   }];
