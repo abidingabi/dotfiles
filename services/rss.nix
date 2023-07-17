df9f901503809e27944985dc3d68253503e1b913
Abigail <abigail@dogbuilt.net>
2023-07-17 17:07:38 -0400

    rss: switch to using tailscale for auth

--- services/rss.nix
+++ services/rss.nix
@@ -7,15 +7,27 @@
     adminCredentialsFile = "/etc/secrets/miniflux-admin-credentials";
 
     config = {
-      BASE_URL = "https://rss.priv.dogbuilt.net/";
+      BASE_URL = "http://rss.priv.dogbuilt.net/";
       CREATE_ADMIN = "1";
       LISTEN_ADDR = "127.0.0.1:4001";
+
+      AUTH_PROXY_HEADER="X-Webauth-User";
     };
   };
 
+  # see https://caddyserver.com/docs/caddyfile/directives/forward_auth#tailscale
   services.caddy.extraConfig = ''
-    http://rss.priv.dogbuilt.net rss.priv.dogbuilt.net {
-      tls internal
+    http://rss.priv.dogbuilt.net {
+      forward_auth unix//run/tailscale.nginx-auth.sock {
+        uri /auth
+        header_up Remote-Addr {remote_host}
+        header_up Remote-Port {remote_port}
+        header_up Original-URI {uri}
+        copy_headers {
+          Tailscale-User>X-Webauth-User
+        }
+      }
+
       encode zstd gzip
       reverse_proxy localhost:4001
     }
--- services/tailscale.nix
+++ services/tailscale.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ pkgs, config, lib, ... }:
 
 {
   options.dogbuilt.services.tailscale = {
@@ -8,7 +8,29 @@
 
   config = let cfg = config.dogbuilt.services.tailscale;
   in {
-    services.tailscale.enable = true;
+    services.tailscale = {
+      enable = true;
+
+      # Builds support for nginx-auth, see https://github.com/NixOS/nixpkgs/issues/227380.
+      package = pkgs.tailscale.override {
+        buildGoModule = args:
+          pkgs.buildGoModule (args // {
+            subPackages = args.subPackages ++ [ "cmd/nginx-auth" ];
+            postInstall = args.postInstall + ''
+              sed -i -e "s#/usr/sbin/tailscale.#$out/bin/#" ./cmd/nginx-auth/tailscale.nginx-auth.service
+              install -D -m0444 -t $out/lib/systemd/system ./cmd/nginx-auth/tailscale.nginx-auth.service
+              sed -i -e "s#/var/run#/run#" ./cmd/nginx-auth/tailscale.nginx-auth.socket
+              sed -i -e "/^Wants/d" ./cmd/nginx-auth/tailscale.nginx-auth.service
+              sed -i -e "/^After/d" ./cmd/nginx-auth/tailscale.nginx-auth.service
+              install -D -m0444 -t $out/lib/systemd/system ./cmd/nginx-auth/tailscale.nginx-auth.socket
+            '';
+          });
+      };
+    };
+    systemd.sockets."tailscale.nginx-auth".wantedBy = [ "multi-user.target" ];
+    systemd.services."tailscale.nginx-auth".after = [ ];
+    systemd.services."tailscale.nginx-auth".wants = [ ];
+
     networking.firewall = {
       checkReversePath = "loose";
       trustedInterfaces = [ "tailscale0" ];
