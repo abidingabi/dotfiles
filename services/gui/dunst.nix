231101dcd340328d7d7116a0e22b2ebedd55a26c
Abigail <abigail@dogbuilt.net>
2024-07-07 01:04:01 -0400

    dunst: ignore Lollypop notifications

--- services/gui/dunst.nix
+++ services/gui/dunst.nix
@@ -6,6 +6,11 @@
         transparency = 10;
         geometry = "700-30+20";
       };
+
+      ignored = {
+        desktop_entry = "org.gnome.Lollypop";
+        skip_display = true;
+      };
     };
   };
 }
