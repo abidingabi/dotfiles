9b31685913e60d2610259819db0939bffa227fac
abigail <abigail@dogbuilt.net>
2025-04-24 00:41:32 -0400

    allium: get hardware video acceleration working

--- devices/allium.nix
+++ devices/allium.nix
@@ -27,6 +27,19 @@
   # hardware config
   hardware.enableRedistributableFirmware = true;
 
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  }; # Force intel-media-driver
+
   boot.initrd.availableKernelModules = [
     "xhci_pci"
     "ahci"
