e76bd3190921b73a559962b04acf859afb93280f
abigail <abigail@dogbuilt.net>
2025-05-24 13:09:30 -0400

    steam: get rid of deprecation warning

--- applications/steam.nix
+++ applications/steam.nix
@@ -3,6 +3,6 @@
 {
   hardware.graphics.enable32Bit = true;
   hardware.graphics.extraPackages32 = [ pkgs.pkgsi686Linux.libva ];
-  hardware.pulseaudio.support32Bit = true;
+  services.pulseaudio.support32Bit = true;
   programs.steam.enable = true;
 }
