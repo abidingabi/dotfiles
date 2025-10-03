27d845786e2dd30507a3c5fc6a5fba5b3c4ad85c
abigail <abigail@dogbuilt.net>
2025-10-03 14:06:41 -0400

    base: disable nix channels

--- devices/base.nix
+++ devices/base.nix
@@ -18,6 +18,9 @@ in {
         auto-optimise-store = true;
         experimental-features = [ "nix-command" "flakes" ];
       };
+
+      channel.enable = false;
+      };
     };
 
     nixpkgs.config.allowUnfree = true;
