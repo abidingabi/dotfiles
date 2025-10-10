6ad0d2d4ddc4f34e2ebd87525dad1d19b7c2172f
abigail <abigail@dogbuilt.net>
2025-10-09 21:15:36 -0400

    Revert "libinput: enable disable while typing for touchpad"
    This reverts commit 55093e54a7f3dc572d311f9fd3bdf1ad519d582c.
    
    This breaks playing video games where one is expected to move the mouse
    at the same time as pressing keys on the keyboard such as minecraft.

--- services/gui/input-devices.nix
+++ services/gui/input-devices.nix
@@ -3,7 +3,6 @@
 {
   services.libinput.enable = true;
   services.libinput.mouse.accelProfile = "flat";
-  services.libinput.touchpad.disableWhileTyping = true;
 
   # keyboard options
   services.xserver.xkb = {
