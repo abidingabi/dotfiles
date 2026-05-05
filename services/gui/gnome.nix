54c61c034ed81076e2f954075d4d6f402bafd763
Abigail Goz <abigail@dogbuilt.net>
2026-05-05 00:23:45 -0400

    GNOME: add application launching keybinds

--- services/gui/gnome.nix
+++ services/gui/gnome.nix
@@ -19,6 +19,36 @@
         "settings/file-chooser/clock-format" = "12h";
         "desktop/interface/clock-show-weekday" = true;
 
+        # Custom keybinds. Figure out how to make this nicer if you plan on
+        # adding more (but probably just don’t add more).
+        "settings-daemon/plugins/media-keys/custom-keybindings" = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/browser/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/editor/"
+        ];
+
+        "settings-daemon/plugins/media-keys/custom-keybindings/terminal/name" =
+          "Terminal";
+        "settings-daemon/plugins/media-keys/custom-keybindings/terminal/command" =
+          "kitty";
+        "settings-daemon/plugins/media-keys/custom-keybindings/terminal/binding" =
+          "<Super>Return";
+
+        "settings-daemon/plugins/media-keys/custom-keybindings/browser/name" =
+          "Browser";
+        "settings-daemon/plugins/media-keys/custom-keybindings/browser/command" =
+          "firefox";
+        "settings-daemon/plugins/media-keys/custom-keybindings/browser/binding" =
+          "<Shift><Super>Return";
+
+        "settings-daemon/plugins/media-keys/custom-keybindings/editor/name" =
+          "Editor";
+        "settings-daemon/plugins/media-keys/custom-keybindings/editor/command" =
+          "emacsclient -c";
+        "settings-daemon/plugins/media-keys/custom-keybindings/editor/binding" =
+          "<Super>e";
+
+        # Apps
         # System Manager
         "gnome-system-monitor/solaris-mode" =
           false; # Do not divide CPU usage by CPU count
