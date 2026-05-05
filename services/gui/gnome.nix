ad1686dbd9d456d6b7cc7ed06b74e30993a9c25a
Abigail Goz <abigail@dogbuilt.net>
2026-05-04 23:52:38 -0400

    GNOME: add some basic settings

--- services/gui/gnome.nix
+++ services/gui/gnome.nix
@@ -1,5 +1,28 @@
-{
+{ lib, ... }: {
 
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+
+  home-manager.users.abi = {
+    dconf.enable = true;
+    dconf.settings = {
+      "org/gnome" = {
+        # inputs
+        "input-sources/sources" = [
+          (lib.gvariant.mkTuple [ "xkb" "us+colemak" ])
+          (lib.gvariant.mkTuple [ "xkb" "us" ])
+        ];
+
+        # UI things
+        "desktop/interface/show-battery-percentage" = true;
+        "desktop/interface/clock-format" = "12h";
+        "settings/file-chooser/clock-format" = "12h";
+        "desktop/interface/clock-show-weekday" = true;
+
+        # System Manager
+        "gnome-system-monitor/solaris-mode" =
+          false; # Do not divide CPU usage by CPU count
+      };
+    };
+  };
 }
