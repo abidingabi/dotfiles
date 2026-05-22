9b0c21c0609eec0324dd25a33ef80509b8605e67
Abigail Goz <abigail@dogbuilt.net>
2026-05-22 14:20:02 -0400

    rss: remove tailscale auth

--- services/nixos/rss.nix
+++ services/nixos/rss.nix
@@ -9,31 +9,12 @@
     config = {
       BASE_URL = "http://rss.priv.dogbuilt.net/";
       LISTEN_ADDR = "127.0.0.1:4001";
-
-      AUTH_PROXY_HEADER = "X-Webauth-User";
-      TRUSTED_REVERSE_PROXY_NETWORKS = "100.64.0.0/24";
     };
-  };
-
-  services.tailscaleAuth = {
-    enable = true;
-    user = config.services.caddy.user;
-    group = config.services.caddy.group;
   };
 
   # see https://caddyserver.com/docs/caddyfile/directives/forward_auth#tailscale
   services.caddy.extraConfig = ''
     http://rss.priv.dogbuilt.net {
-      forward_auth unix//run/tailscale-nginx-auth/tailscale-nginx-auth.sock {
-        uri /auth
-        header_up Remote-Addr {remote_host}
-        header_up Remote-Port {remote_port}
-        header_up Original-URI {uri}
-        copy_headers {
-          Tailscale-User>X-Webauth-User
-        }
-      }
-
       encode zstd gzip
       reverse_proxy localhost:4001
     }
